c7e774c388ac85e8bcee40e0a1976c8794746152
Aliy_fl50@itstep.edu.az <Aliy_fl50@itstep.edu.az>
2021-04-15 17:47:31 +0400

    Add project files.

---
+++ ClassWork6/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassWork6
+{
+    public class Car : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string model;
+
+
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value;
+                OnPropertyChanged(); }
+        }
+
+        private void OnPropertyChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string vendor;
+
+
+
+        public string Vendor
+        {
+            get { return vendor; }
+            set { vendor = value; }
+        }
+        private int year;
+
+
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+
+    }
+}
